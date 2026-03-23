28fdff279c492c6da92137cda577603887c5ca92
z0hra1n <zareena286@gmail.com>
2026-03-23 19:44:08 +0530

    Added the firmware.

---
+++ firmware/ir.ino
@@ -0,0 +1,83 @@
+#include <Wire.h>
+#include <BleMouse.h>
+
+BleMouse bleMouse;
+
+#define triggerPin 25
+#define reloadPin 26
+#define solenoidPin 27
+
+int IRsensorAddress = 0xB0;
+int slaveAddress = IRsensorAddress >> 1;
+
+byte data_buf[16];
+int Ix[4];
+int Iy[4];
+int s;
+
+bool solenoid = false;
+
+void Write_2bytes(byte d1, byte d2){
+  Wire.beginTransmission(slaveAddress);
+  Wire.write(d1); Wire.write(d2);
+  Wire.endTransmission();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  pinMode(triggerPin, INPUT_PULLUP);
+  pinMode(reloadPin, INPUT_PULLUP);
+  pinMode(solenoidPin, OUTPUT);
+  digitalWrite(solenoidPin, LOW);
+  bleMouse.begin();
+  Write_2bytes(0x30,0x01); delay(10);
+  Write_2bytes(0x30,0x08); delay(10);
+  Write_2bytes(0x06,0x90); delay(10);
+  Write_2bytes(0x08,0xC0); delay(10);
+  Write_2bytes(0x1A,0x40); delay(10);
+  Write_2bytes(0x33,0x33); delay(10);
+  delay(100);
+}
+
+void loop() {
+  solenoid = digitalRead(triggerPin) == HIGH;
+  digitalWrite(solenoidPin, solenoid ? HIGH : LOW);
+
+  if(solenoid && bleMouse.isConnected()) bleMouse.press(MOUSE_LEFT);
+  else if(bleMouse.isConnected()) bleMouse.release(MOUSE_LEFT);
+
+  if(digitalRead(reloadPin) == HIGH && bleMouse.isConnected()){
+    bleMouse.press(KEY_R);
+    delay(100);
+    bleMouse.release(KEY_R);
+  }
+
+  Wire.beginTransmission(slaveAddress);
+  Wire.write(0x36);
+  Wire.endTransmission();
+  Wire.requestFrom(slaveAddress, 16);
+
+  int i = 0;
+  while(Wire.available() && i < 16){
+    data_buf[i] = Wire.read();
+    i++;
+  }
+
+  Ix[0] = data_buf[1];
+  Iy[0] = data_buf[2];
+  s = data_buf[3];
+  Ix[0] += (s & 0x30) << 4;
+  Iy[0] += (s & 0xC0) << 2;
+
+  Serial.print("X="); Serial.print(Ix[0]);
+  Serial.print(" Y="); Serial.println(Iy[0]);
+
+  if(bleMouse.isConnected()){
+    int deltaX = map(Ix[0], 0, 1023, -5, 5);
+    int deltaY = map(Iy[0], 0, 767, -5, 5);
+    bleMouse.move(deltaX, deltaY);
+  }
+
+  delay(50);
+}
